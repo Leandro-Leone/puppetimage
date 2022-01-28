2ab3b176f541979288279080c5e3c7fa2a331376
Copecofee <84367018+Copecofee@users.noreply.github.com>
2022-01-28 12:55:22 -0300

    Update puppet.py

--- puppet.py
+++ puppet.py
@@ -1,14 +1,23 @@
+import time
 import os
+from colorama import *
 
 os.system("clear")
-photos = int(input("How many pictures do you want to upload? 1/100 "))
 
-while photos < 100:
-  os.system("wget https://www.thispersondoesnotexist.com/image")
-  photos = photos - 1
-  if photos == 0:
-   print('hey') 
-   break
-  #else:
-   #continue
+def run():
+ photos = int(input("How many pictures do you want to upload? 1/100 "))
 
+ if photos < 101:
+  pass
+ else:
+  os.system("clear")
+  exit(Fore.RED+"MAX uploads is 100")
+
+ for i in range(photos):
+   os.system("wget https://www.thispersondoesnotexist.com/image")
+
+def main():
+ run()
+
+if __name__ == '__main__':
+ main()
